d23fc677273f7a8c9fea6ff5d8e7d50fd21eb850
Adam Kissaglis <adam.kissaglis@cloudcall.com>
2018-01-06 23:17:46 +0000

    CT1 - Add colour and recent change values

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const request = require('request-promise');
-const coins = require("./config/coins.json");
+const coins = require('./config/coins.json');
 
-const uri = "https://api.coinmarketcap.com/v1/ticker/";
+const uri = 'https://api.coinmarketcap.com/v1/ticker/';
 const currentPrices = [];
 const pricePromises = [];
+const fontColor = {
+    down: '33m\u2193 ',
+    up: '32m\u2191 '
+}
+
+const resultsPrint =[];
 
 async function requestCoinData(coin){
     return request(uri + coin.name)
     .then(results => {
         results = JSON.parse(results);
-        currentPrices.push({ name: coin.name, price: results[0].price_usd });
+        currentPrices.push(
+            {
+                name: coin.name, 
+                price: results[0].price_usd           
+            }
+        );
+        resultsPrint.push(results);
+
+        coin.change1hr = results[0].percent_change_1h;
+        coin.change1hrArrow =  results[0].percent_change_1h > 0 ? fontColor.up : fontColor.down;
+        coin.change24hr = results[0].percent_change_24h;
+        coin.change24hrArrow =  results[0].percent_change_24h > 0 ? fontColor.up : fontColor.down;
+        coin.change7d = results[0].percent_change_7d;
+        coin.change7dArrow = results[0].percent_change_7d > 0 ? fontColor.up : fontColor.down;
+
         for (let i = 0; i < coins.length; i++) {
             if (coins[i].name === currentPrice.name) {
                 coins[i].currentPrice = results[0].price_usd;
@@ -17,7 +37,7 @@ async function requestCoinData(coin){
         }
     })
     .catch(() => {
-         return "error retrieving data";
+         return 'error retrieving data';
     })
 }
 
@@ -25,13 +45,14 @@ function calculateProfit(currentPrice) {
     for (let i = 0; i < coins.length; i++) {
         if (coins[i].name === currentPrice.name) {
             let coin = coins[i];
-            let changeInValue = (currentPrice.price - coin.purchasePrice) * 100;
+            let changeInValue = (currentPrice.price / coin.purchasePrice) * 100;
             let profitMade = coin.volume * (changeInValue / 100);
             coin.currentPrice = currentPrice.price;
             coin.changeInValue = parseFloat(changeInValue).toFixed(5);
             coin.originalValue = parseFloat(coin.purchasePrice * coin.volume).toFixed(2);
             coin.currentValue = parseFloat(coin.currentPrice * coin.volume).toFixed(2);
             coin.profitMade = parseFloat(coin.currentValue - coin.originalValue).toFixed(2);
+            coin.profitArrow = coin.profitMade < 0 ? fontColor.down : fontColor.up;
         }
     }
 }
@@ -50,23 +71,20 @@ Promise.all(pricePromises)
         for (let i = 0; i < coins.length; i++) {
             let coin = coins[i];
             console.log(coin.name);
-            console.log("original price: $" + coin.purchasePrice);
-            console.log("current price: $" + coin.currentPrice);
-            console.log("original value: $" + coin.originalValue);
-            console.log("current value: $" + coin.currentValue);
-            console.log("Change in price = " + coin.changeInValue + "%");
-            console.log("Profit made = $" + coin.profitMade + "\n");
+            console.log('original price: $' + coin.purchasePrice);
+            console.log('current price: ' + coin.currentPrice);
+            console.log('change in 1hr: \x1b[' + coin.change1hrArrow + '1hr $' + coin.change1hr + '%\x1b[0m');
+            console.log('change in 24hr: \x1b[' + coin.change24hrArrow + '24hr $' + coin.change24hr + '%\x1b[0m');
+            console.log('change in 7d: \x1b[' + coin.change1hrArrow + '7d $' + coin.change7d + '%\x1b[0m');
+            console.log('original value: $' + coin.originalValue);
+            console.log('current value: $' + coin.currentValue);
+            console.log('Change in price: ' + coin.changeInValue + '%');
+            console.log('Profit made: \x1b[' + coin.profitArrow + '$' + coin.profitMade + '\x1b[0m\n');;
             totalProfit += Number(coin.profitMade);
         }
-        console.log("\n$" + parseFloat(totalProfit).toFixed(2));
+        // console.log(resultsPrint);
+        console.log('\n$' + parseFloat(totalProfit).toFixed(2));
     })
     .catch(err => {
         console.log(err);
     });
-
-
-// request = promises
-// fulfill promises and then calculate results
-// Then show results
-
-
